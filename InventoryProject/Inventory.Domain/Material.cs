ab51eea6bc34a15b8dfb43fa659a5adfc10ef9f8
지훈 허 <jhhu@yullin.com>
2019-12-16 15:46:34 +0900

    모델 추가

---
+++ InventoryProject/Inventory.Domain/Material.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Domain
+{
+    public class Material
+    {
+        public int matNo { get; set; }
+        public string matNm { get; set; }
+        public string itemNo { get; set; }
+        public string rmk { get; set; }
+        public string grpCd { get; set; }
+        public string subCd { get; set; }
+        public string unitCd { get; set; }
+    }
+}
---
+++ InventoryProject/Inventory.Domain/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Domain
+{
+    public class Unit
+    {
+        public string unitCd { get; set; }
+        public string unitNm { get; set; }
+        public string rmk { get; set; }
+
+    }
+}
