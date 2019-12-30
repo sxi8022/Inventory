b8993214dc11a6747a0491fe778f783c5e2874b5
DK <dkkang@yullin.com>
2019-12-30 10:19:10 +0900

    1

---
+++ InventoryProject/WebApi/Controllers/GroupController.cs
@@ -0,0 +1,49 @@
+﻿using Inventory.Domain;
+using Inventory.Service;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web;
+
+namespace WebApi.Controllers
+{
+    public class GroupController : ApiController
+    {
+        Query qry = new Query();
+
+        public List<MatGrp> Get()
+        {
+            return qry.SelectMatGrp();
+        }
+
+        //public JsonResult MaterialGrpSearch()
+        //{
+        //    List<MatGrp> grpList = query.SelectMatGrp();
+        //    return Json(grpList, JsonRequestBehavior.AllowGet);
+        //}
+
+
+        public void Post(MatGrp pMat)
+        {
+            if (pMat == null)
+                return;
+
+            qry.InsertMatGrp(pMat.grpNm, pMat.rmk);
+        }
+
+        public void Put(MatGrp pMat)
+        {
+            if (pMat.grpCd == null)
+                return;
+
+            qry.UpdatetMatGrp(pMat.grpCd, pMat.grpNm, pMat.rmk);
+        }
+
+        public void Delete(MatGrp pMat)
+        {
+            if (pMat.grpCd == null)
+                return;
+
+            qry.DeleteMatGrp(pMat.grpCd);
+        }
+    }
+}
---
+++ InventoryProject/WebApi/Controllers/SubGroupController.cs
@@ -0,0 +1,19 @@
+﻿using Inventory.Domain;
+using Inventory.Service;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web;
+
+namespace WebApi.Controllers
+{
+    public class SubGroupController : ApiController
+    {
+        Query qry = new Query();
+
+        public List<MatGrp> Get(string id)
+        {
+            return qry.SelectMatGrpSub(id);
+        }
+
+    }
+}
