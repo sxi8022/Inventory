cfc5267a29e574a0c9d372bab9d4b34c116f8977
sxi8022 <sxi8022@gmail.com>
2019-12-30 17:49:42 +0900

    로그인모델추가(테스트용)

---
+++ InventoryProject/Inventory.Web/Models/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory.Web.Models
+{
+    public class LoginViewModel
+    {
+        /// <summary>
+        /// 사용자id
+        /// </summary>
+        public string userId { get; set; }
+
+        /// <summary>
+        /// 사용자비밀번호
+        /// </summary>
+        public string passWord { get; set; }
+
+        /// <summary>
+        /// 로그인상태
+        /// </summary>
+        public string state { get; set; }
+    }
+}
---
+++ InventoryProject/WebApi/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Inventory.Service;
+using Inventory.Web.Models;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    /// <summary>
+    /// 사용자
+    /// </summary>
+    public class AccountController : ApiController
+    {
+        Query query = new Query();
+
+        /// <summary>
+        /// 로그인
+        /// </summary>
+        /// <param name="userid"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public LoginViewModel Post(string userid, string password)
+        {
+            LoginViewModel model = new LoginViewModel();
+
+            if (userid == "admin" && password == "1")
+            {
+                model.userId = "admin";
+                model.passWord = "1";
+                model.state = "SUCCESS";
+            }
+            else
+            {
+                model.userId = string.Empty;
+                model.passWord = string.Empty;
+                model.state = "FAIL";
+            }
+
+            return model;
+        }
+    }
+}
