6ee03063a37812cb95dc49c300ef226f8f29d3af
sxi8022 <sxi8022@gmail.com>
2020-01-02 17:01:56 +0900

    모델수정

---
+++ InventoryProject/Inventory.WebApi/Models/LoginViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory.WebApi.Models
+{
+    public class LoginViewModel
+    {
+        /// <summary>
+        /// 사용자id
+        /// </summary>
+        public string userId { get; set; }
+
+        /// <summary>
+        /// 사용자비밀번호
+        /// </summary>
+        public string passWord { get; set; }
+
+        /// <summary>
+        /// 로그인상태
+        /// </summary>
+        public string state { get; set; }
+
+        /// <summary>
+        /// 키값
+        /// </summary>
+        public string formKey { get; set; }
+    }
+}
